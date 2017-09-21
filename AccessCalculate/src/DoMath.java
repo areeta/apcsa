7de9ccd3d3a423c5ac07c909d28e42c678660e32
Areeta Wong <areetaw@gmail.com>
2017-09-21 16:35:50 -0700

    Part 5

--- AccessCalculate/src/DoMath.java
+++ AccessCalculate/src/DoMath.java
@@ -10,15 +10,14 @@ public class DoMath {
 //		System.out.println(Calculate.toRadians(10));
 //		System.out.println(Calculate.toMixedNum(7, 2));
 //		System.out.println(Calculate.toImproperFrac(3, 1, 2));
-//		System.out.println(Calculate.foil(2, 3, 6, -7, "n" ));
+		System.out.println(Calculate.foil(2, 3, 6, -7, "n" ));
 //		System.out.println(Calculate.round2(-233.1588));
 //		System.out.println(Calculate.exponent(2, 3));
 //		System.out.println(Calculate.factorial(-8));
 //		System.out.println(Calculate.isPrime(2));
 //		System.out.println(Calculate.gcf(2, 4));
 //		System.out.println(Calculate.sqrt(121));
-		System.out.println(Calculate.quadForm(1, 4, 4));
-		System.out.println(Calculate.round2(135234.34234));
+		System.out.println(Calculate.quadForm(-5, -4, 39));
 
 	}
 }
